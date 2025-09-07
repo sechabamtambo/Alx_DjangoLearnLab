20b30495867749da1a8f850c160f13f508fec025
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-07 22:46:56 +0200

    made changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Book and Library views
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-    # Book permission-based views
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/', views.edit_book, name='edit_book'),
-    path('books/delete/', views.delete_book, name='delete_book'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/edit_book/', views.edit_book, name='edit_book'),
+    path('books/delete_book/', views.delete_book, name='delete_book'),
 ]
