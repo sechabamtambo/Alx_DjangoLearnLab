1df02c6aa28a22f3e3f8fcf621afb8e4cadd01a4
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-07 20:47:58 +0200

    Fix views.py to use correct template paths for ALX checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Library
 from django.views.generic import DetailView
-from .models import Library
+from .models import Book, Library
+
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     
-    # Optional: render HTML template
+    # Render using template in the app folder for ALX compliance
     try:
-        return render(request, 'list_books.html', {'books': books})
+        return render(request, 'relationship_app/list_books.html', {'books': books})
     except:
-        # fallback to plain text
+        # Fallback to plain text if template not found
         book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
         return HttpResponse(f"<pre>{book_list}</pre>")
-    
+
+# Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # ALX requires app folder
     context_object_name = 'library'
