9e1fb4c29c4df4ad453913869790597b53a22330
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-07 20:51:38 +0200

    Fix views.py: explicit Library import for ALX compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library  
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    
-    # Render using template in the app folder for ALX compliance
+
     try:
         return render(request, 'relationship_app/list_books.html', {'books': books})
     except:
-        # Fallback to plain text if template not found
         book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
         return HttpResponse(f"<pre>{book_list}</pre>")
 
-# Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # ALX requires app folder
+    template_name = 'relationship_app/library_detail.html'  
     context_object_name = 'library'
