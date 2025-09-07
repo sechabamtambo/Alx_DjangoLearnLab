98b3f3d3aee285c2217f34d99bb73a67e98b31a7
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-07 20:56:36 +0200

    Fix DetailView import from django.views.generic.detail for ALX

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
-from .models import Library  
+from .models import Library
 
 def list_books(request):
     books = Book.objects.all()
-
     try:
         return render(request, 'relationship_app/list_books.html', {'books': books})
     except:
@@ -15,5 +14,5 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
