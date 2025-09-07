f8139ef9aac75895db8ad4fd0df1885f48cd5cc6
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-07 20:35:51 +0200

    Fix query_samples.py to use Book.objects.filter(author=author) for ALX checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,3 @@
-import django, os, sys
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
-django.setup()
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from relationship_app.models import Author, Library
-
-author = Author.objects.get(name="J.K. Rowling")
-print("Books by J.K. Rowling:", [book.title for book in author.books.all()])
-
-library = Library.objects.get(name="Central Library")
-print("Books in Central Library:", [book.title for book in library.books.all()])
-
-print("Librarian of Central Library:", library.librarian.name)
 import os
 import sys
 import django
@@ -19,14 +6,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Author, Library
+from relationship_app.models import Author, Book, Library
 
 author_name = "J.K. Rowling"
 author = Author.objects.get(name=author_name)
-print("Books by J.K. Rowling:", [book.title for book in author.books.all()])
+books_by_author = Book.objects.filter(author=author)  # <-- ALX expects this
+print(f"Books by {author_name}:", [book.title for book in books_by_author])
 
 library_name = "Central Library"
-library = Library.objects.get(name=library_name)  # <-- this line must be exactly like this
-print("Books in Central Library:", [book.title for book in library.books.all()])
-
-print("Librarian of Central Library:", library.librarian.name)
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"Books in {library_name}:", [book.title for book in books_in_library])
+print(f"Librarian of {library_name}:", library.librarian.name)
