35fe1498d94769fed492de36a85d18582909fdae
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-21 10:06:11 +0200

    feat(api): add BookViewSet with CRUD operations and register it with DefaultRouter

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,6 +6,6 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls)),  # All CRUD routes
+    path('books/', BookList.as_view(), name='book-list'),  # ListAPIView
+    path('', include(router.urls)),  # Include all CRUD routes from the router
 ]
