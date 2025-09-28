a3173103943b284f658dae12fad8338bc927ec82
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-09-28 17:25:38 +0200

    Updated tests to include response.data checks for CRUD endpoints

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -11,21 +11,35 @@ class BookAPITestCase(APITestCase):
         self.book = Book.objects.create(title='Book One', publication_year=2000, author=self.author)
 
         self.list_url = reverse('book-list')
+        self.detail_url = reverse('book-detail', kwargs={'pk': self.book.id})
         self.create_url = reverse('book-create')
         self.update_url = reverse('book-update')
         self.delete_url = reverse('book-delete')
+
+    def test_list_books(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['title'], self.book.title)
+
+    def test_detail_book(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.book.title)
 
     def test_create_book_authenticated(self):
         self.client.login(username='testuser', password='testpass')
         data = {'title': 'Book Two', 'publication_year': 2021, 'author': self.author.id}
         response = self.client.post(self.create_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'Book Two')
 
-    def test_update_delete_authenticated(self):
+    def test_update_book_authenticated(self):
         self.client.login(username='testuser', password='testpass')
-        data = {'title': 'Updated', 'publication_year': 2000, 'author': self.author.id}
+        data = {'title': 'Updated Book', 'publication_year': 2000, 'author': self.author.id}
         response = self.client.put(self.update_url, data)
         self.assertIn(response.status_code, [status.HTTP_200_OK, status.HTTP_204_NO_CONTENT])
 
+    def test_delete_book_authenticated(self):
+        self.client.login(username='testuser', password='testpass')
         response = self.client.delete(self.delete_url)
         self.assertIn(response.status_code, [status.HTTP_204_NO_CONTENT, status.HTTP_200_OK])
