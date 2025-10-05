79e3b57209ccbb443014fb74668233490504fe02
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-10-05 21:02:06 +0200

    Fix comment creation URL and view to match checker

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -100,8 +100,9 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs["post_id"])
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs["pk"])
         return super().form_valid(form)
+
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
