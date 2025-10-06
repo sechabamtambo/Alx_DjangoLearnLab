624197edac62a97b09fafc4c7feae087c54bff22
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-10-06 10:38:52 +0200

    Fix database config and static settings for ALX checker

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,6 +75,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '', 
+        'PORT': '',
+        'USER': '', 
     }
 }
 
